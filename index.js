b25fcb04e8ed64a93468a5abccfbeef7cd0480fb
francienevaz <fran.vfraga@gmail.com>
2024-01-06 13:23:25 -0300

    updated

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express';
+import consign from 'consign';
+// const router = express.Router();
 const PORT = 3000;
 
 const app = express()
 
 app.set("json spaces", 4)
 
-app.get("/", (req, res) => res.json({status: "TaskApi"}));
+consign()
+.include("models")
+.then("routes")
+.into(app);
 
-app.get("/tasks", (req, res) => {
-    res.json({
-        tasks: [
-            {title: "Fazer compras"},
-            {title: "Fazer compras"},
-            {title: "Trocar entrada SSD"},
-        ]
-    })
-})
-
-app.listen(PORT, () => console.log("TaskApi - working on port ${PORT}"));
+app.listen(PORT, () => console.log(`TaskApi - working on port ${PORT}`));
